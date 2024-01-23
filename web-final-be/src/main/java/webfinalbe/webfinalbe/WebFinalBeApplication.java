73c8a01faf227423a5d302066e598fd197ef0377
Gökay Şahin <59969301+g0k4yy@users.noreply.github.com>
2024-01-23 18:46:34 +0300

    Add files via upload

---
+++ web-final-be/src/main/java/webfinalbe/webfinalbe/WebFinalBeApplication.java
@@ -0,0 +1,13 @@
+package webfinalbe.webfinalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFinalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFinalBeApplication.class, args);
+	}
+
+}
